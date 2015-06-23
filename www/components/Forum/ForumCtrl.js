86ddd0707db626b2ace70eaff709e314f39153b8
bdstein33 <bdstein33@gmail.com>
2015-06-23 12:05:49 -0700

    Fixed question directive

--- www/components/Forum/ForumCtrl.js
+++ www/components/Forum/ForumCtrl.js
@@ -1,22 +1,22 @@
-app.controller('ForumCtrl', ['$scope', function($scope) {
+app.controller('ForumCtrl', ['$scope', 'ForumsFactory', function($scope, ForumsFactory) {
   $scope.title = "Example Forum";
 
   $scope.dummyData = [
     {
       id: "0",
-      question: 'Can you please explain why we need to use an asynchronous callback in the function?',
+      text: 'Can you please explain why we need to use an asynchronous callback in the function?',
       name: 'Ben Steinberg',
       rank: 0
     },
     {
       id: "1",
-      question: 'Can you please explain why we need to use an asynchronous callback in the function?  Can you please explain why we need to use an asynchronous callback in the function?',
+      text: 'Can you please explain why we need to use an asynchronous callback in the function?  Can you please explain why we need to use an asynchronous callback in the function?',
       name: 'Amy Steinberg',
       rank: 0
     },
     {
       id: "2",
-      question: 'Can you please explain why we need to use an asynchronous callback in the function?',
+      text: 'Can you please explain why we need to use an asynchronous callback in the function?',
       name: 'John Smith',
       rank: 0
     },
@@ -30,56 +30,58 @@ app.controller('ForumCtrl', ['$scope', function($scope) {
     $scope.dummyData[parseInt(id)].rank--;
   };
 
+
+
+  
+
+
+
 }]);
 
 
 
 
+// app.directive('ngQuestion', function() {
+//   return {
+//     controller: function($scope) {}
+//   };
+// });
+// app.directive('ngName', function() {
+//   return {
+//     controller: function($scope) {}
+//   };
+// });
+// app.directive('ngRank', function() {
+//   return {
+//     controller: function($scope) {}
+//   };
+// });
+
+
 app.directive('ngQuestion', function() {
   return {
-    controller: function($scope) {}
-  };
-});
-app.directive('ngName', function() {
-  return {
-    controller: function($scope) {}
-  };
-});
-app.directive('ngRank', function() {
-  return {
-    controller: function($scope) {}
-  };
-});
-
-
-app.directive('question', function() {
-  return {
-    restrict: 'AE',
-    require: '?ngQuestion, ?ngName, ?ngRank',
+    restrict: 'E',
     scope: {
-      ngQuestion: '@',
-      ngName: '@',
-      ngRank: '@'
+      question: '='
     },
     template: '<div class="right-content">' +
   '<div class="up arrow-container" ng-click="upVote()"></div>' +
-  '<div class="rank-container">{{ngRank}}</div>' +
+  '<div class="rank-container">{{question.rank}}</div>' +
   '<div class="down arrow-container" ng-click="downVote()"></div>' +
   '</div>  ' +
  ' <div class="left-content">' +
     '<div class="question-text-container">' +
-     ' <p>{{ngQuestion}}</p>' +
+     ' <p>{{question.text}}</p>' +
     '</div>' +
-    '<p class="question-name">{{ngName}}</p>' +
+    '<p class="question-name">{{question.name}}</p>' +
   '</div>',
-   link: function($scope, element, attribute) {
-      $scope.upVote = function() {
-        $scope.ngRank++;
-        console.log($scope.dummyData);
+   link: function(scope, element, attribute) {
+      scope.upVote = function() {
+        scope.question.rank++;
       };
 
-      $scope.downVote = function() {
-        $scope.ngRank--;
+      scope.downVote = function() {
+        scope.question.rank--;
       };
     }
    
