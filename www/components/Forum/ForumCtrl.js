67517a94398590ea38aabec47c4a956aa970d91e
bdstein33 <bdstein33@gmail.com>
2015-06-23 15:40:20 -0700

    Add functionality to pass questions from pending to active to answered

--- www/components/Forum/ForumCtrl.js
+++ www/components/Forum/ForumCtrl.js
@@ -5,21 +5,21 @@ app.controller('ForumCtrl', ['$scope', 'ForumsFactory', function($scope, ForumsF
   $scope.forum = {JsWjcMAbUt4dFbqBQUn: {createdAt: "", creatorID: "simplelogin:1", private: false, questions: {
     pending: [{
       id: "0",
-      text: 'Can you please explain why we need to use an asynchronous callback in the function?',
+      text: 'AAACan you please explain why we need to use an asynchronous callback in the function?',
       name: 'Ben Steinberg',
       rank: 0,
       status: 'pending'
     },
     {
       id: "1",
-      text: 'Can you please explain why we need to use an asynchronous callback in the function?  Can you please explain why we need to use an asynchronous callback in the function?',
+      text: 'BBBCan you please explain why we need to use an asynchronous callback in the function?  Can you please explain why we need to use an asynchronous callback in the function?',
       name: 'Amy Steinberg',
       rank: 0,
       status: 'pending'
     },
     {
       id: "2",
-      text: 'Can you please explain why we need to use an asynchronous callback in the function?',
+      text: 'CCCCan you please explain why we need to use an asynchronous callback in the function?',
       name: 'John Smith',
       rank: 0,
       status: 'pending'
@@ -35,59 +35,40 @@ app.controller('ForumCtrl', ['$scope', 'ForumsFactory', function($scope, ForumsF
   }};
 
 
-  $scope.fetchQuestions = function() {
-    $scope.pendingQuestions = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending;
-    $scope.activeQuestion = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.active;
-    $scope.answeredQuestions = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.answered;
-  };
+  $scope.pendingQuestions = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending;
+  $scope.activeQuestion = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.active;
+  $scope.answeredQuestions = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.answered;
 
   $scope.nextQuestion = function() {
     console.log("B");
-
-     // If there is an active question, move it to answered questions array
-     if (!!$scope.activeQuestion) {
-      $scope.answeredQuestions.push($scope.activeQuestion);
-      $scope.activeQuestion = null;
-     }
-
-     // Set active question equal to the pending question with the highest ranking
-
-     // If there are no pending questions, set active question equal to null
-
-
-     // if (!!$scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending.length > 0) {
-     //  var nextQuestion = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending[0];
-     //  for (var i = 1; i < $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending.length)
-     // }
-
-    // Update scope data
-    // $scope.fetchQuestions();
-    // console.log($scope.forum.JsWjcMAbUt4dFbqBQUn.questions.answered);
+    $scope.removeActiveQuestion();
+    $scope.getNextActiveQuestion();
   };
 
+  // If there is an active question, move it to answered questions array
+  // and set active question to null
+  $scope.removeActiveQuestion = function() {
+    if (!!$scope.activeQuestion) {
+      $scope.answeredQuestions.push($scope.activeQuestion);
+      $scope.activeQuestion = null;
+    }
+  };
 
-  $scope.fetchQuestions();
-  // setTimeout(function() {
-  //   console.log("A");
-  //   $scope.$apply(function() {
-  //     $scope.nextQuestion();
-  //     console.log($scope.activeQuestion);
-  //   });
-    
-  // }, 2000);
+  // Set the active question to the pending question with the highest rank
+  // if there are any pending questions
+  $scope.getNextActiveQuestion = function() {
+    if ($scope.pendingQuestions.length > 0) {
+      var nextQuestion = $scope.pendingQuestions.shift();
 
-
-  // $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.active.unshift({
-  //     id: "10",
-  //     text: 'This is currently the active question.  Hopefully this will work.',
-  //     name: 'Michael Jordan',
-  //     rank: 10,
-  //     status: 'active'
-  //   })
-  // console.log("A");
-  // console.log($scope.forum.JsWjcMAbUt4dFbqBQUn.questions.active[0]);
-
-
+      for (var i = 0; i < $scope.pendingQuestions.length; i++) {
+        if ($scope.pendingQuestions[i].rank > nextQuestion.rank) {
+          $scope.pendingQuestions.push(nextQuestion);
+          nextQuestion = $scope.pendingQuestions[i];
+        }
+      }
+      $scope.activeQuestion = nextQuestion;
+    }
+  };
 }]);
 
 app.directive('ngPendingQuestion', function() {
@@ -100,6 +81,36 @@ app.directive('ngPendingQuestion', function() {
   '<div class="up arrow-container" ng-click="upVote()"></div>' +
   '<div class="rank-container">{{question.rank}}</div>' +
   '<div class="down arrow-container" ng-click="downVote()"></div>' +
+  '</div>  ' +
+ ' <div class="left-content">' +
+    '<div class="question-text-container">' +
+     ' <p>{{question.text}}</p>' +
+    '</div>' +
+    '<p class="question-name">{{question.name}}</p>' +
+  '</div>',
+   link: function(scope, element, attribute) {
+      scope.upVote = function() {
+        scope.question.rank++;
+      };
+
+      scope.downVote = function() {
+        scope.question.rank--;
+      };
+    }
+   
+  };
+});
+
+app.directive('ngAnsweredQuestion', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      question: '='
+    },
+    template: '<div class="right-content">' +
+  '<div class="up arrow-container active-arrow" ng-click="upVote()"></div>' +
+  '<div class="rank-container active-rank">{{question.rank}}</div>' +
+  '<div class="down arrow-container active-arrow" ng-click="downVote()"></div>' +
   '</div>  ' +
  ' <div class="left-content">' +
     '<div class="question-text-container">' +
