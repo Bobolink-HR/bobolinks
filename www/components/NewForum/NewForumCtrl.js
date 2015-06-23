0420603e557e6a01498292c1b3c3c90e76571be9
Dianna Faulk <diannafaulk@gmail.com>
2015-06-23 13:50:53 -0700

    Use ForumsFactory to save new forum data

--- www/components/NewForum/NewForumCtrl.js
+++ www/components/NewForum/NewForumCtrl.js
@@ -1,3 +1,18 @@
-app.controller('NewForumCtrl', ['$scope', function($scope) {
+app.controller('NewForumCtrl', ['$scope', 'ForumsFactory', function($scope, ForumsFactory) {
   $scope.title = 'Create New Forum';
+
+  $scope.newForum = {};
+
+  $scope.addForum = function(newForum) {
+    $scope.newForum.creatorID = "simplelogin:1"; //Update this to be the actual user ID
+    $scope.newForum.createdAt = JSON.stringify(new Date().toString());
+    console.log(newForum);
+    ForumsFactory.saveForum(newForum).then(function(ref) {
+      var id = ref.key();
+      // $scope.forum = ForumsFactory.getForum(id);
+      console.log("added a new forum with id " + id);
+    }).catch(function(err) {
+      console.error(err);
+    });
+  }
 }]);
