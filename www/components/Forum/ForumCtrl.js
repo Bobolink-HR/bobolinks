c2549c4351306678fc0cf70b25d67e791c0de083
bdstein33 <bdstein33@gmail.com>
2015-06-23 14:02:26 -0700

    Add section for Answered Question.  Complete functionality for removing active question

--- www/components/Forum/ForumCtrl.js
+++ www/components/Forum/ForumCtrl.js
@@ -1,64 +1,96 @@
 app.controller('ForumCtrl', ['$scope', 'ForumsFactory', function($scope, ForumsFactory) {
   $scope.title = "Example Forum";
 
-  $scope.dummyData = [
-    {
+
+  $scope.forum = {JsWjcMAbUt4dFbqBQUn: {createdAt: "", creatorID: "simplelogin:1", private: false, questions: {
+    pending: [{
       id: "0",
       text: 'Can you please explain why we need to use an asynchronous callback in the function?',
       name: 'Ben Steinberg',
-      rank: 0
+      rank: 0,
+      status: 'pending'
     },
     {
       id: "1",
       text: 'Can you please explain why we need to use an asynchronous callback in the function?  Can you please explain why we need to use an asynchronous callback in the function?',
       name: 'Amy Steinberg',
-      rank: 0
+      rank: 0,
+      status: 'pending'
     },
     {
       id: "2",
       text: 'Can you please explain why we need to use an asynchronous callback in the function?',
       name: 'John Smith',
-      rank: 0
-    },
-  ];
+      rank: 0,
+      status: 'pending'
+    }], 
+    active: {
+      id: "6",
+      text: 'This is currently the active question.  Hopefully this will work.',
+      name: 'Michael Jordan',
+      rank: 5,
+      status: 'active'
+    }, 
+    answered: []}
+  }};
 
-  $scope.upVote = function(id){
-    $scope.dummyData[parseInt(id)].rank++;
+
+  $scope.fetchQuestions = function() {
+    $scope.pendingQuestions = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending;
+    $scope.activeQuestion = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.active;
+    $scope.answeredQuestions = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.answered;
   };
 
-  $scope.downVote = function(id) {
-    $scope.dummyData[parseInt(id)].rank--;
+  $scope.nextQuestion = function() {
+    console.log("B");
+
+     // If there is an active question, move it to answered questions array
+     if (!!$scope.activeQuestion) {
+      $scope.answeredQuestions.push($scope.activeQuestion);
+      $scope.activeQuestion = null;
+     }
+
+     // Set active question equal to the pending question with the highest ranking
+
+     // If there are no pending questions, set active question equal to null
+
+
+     // if (!!$scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending.length > 0) {
+     //  var nextQuestion = $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending[0];
+     //  for (var i = 1; i < $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.pending.length)
+     // }
+
+    // Update scope data
+    // $scope.fetchQuestions();
+    // console.log($scope.forum.JsWjcMAbUt4dFbqBQUn.questions.answered);
   };
 
 
+  $scope.fetchQuestions();
+  // setTimeout(function() {
+  //   console.log("A");
+  //   $scope.$apply(function() {
+  //     $scope.nextQuestion();
+  //     console.log($scope.activeQuestion);
+  //   });
+    
+  // }, 2000);
 
-  
 
+  // $scope.forum.JsWjcMAbUt4dFbqBQUn.questions.active.unshift({
+  //     id: "10",
+  //     text: 'This is currently the active question.  Hopefully this will work.',
+  //     name: 'Michael Jordan',
+  //     rank: 10,
+  //     status: 'active'
+  //   })
+  // console.log("A");
+  // console.log($scope.forum.JsWjcMAbUt4dFbqBQUn.questions.active[0]);
 
 
 }]);
 
-
-
-
-// app.directive('ngQuestion', function() {
-//   return {
-//     controller: function($scope) {}
-//   };
-// });
-// app.directive('ngName', function() {
-//   return {
-//     controller: function($scope) {}
-//   };
-// });
-// app.directive('ngRank', function() {
-//   return {
-//     controller: function($scope) {}
-//   };
-// });
-
-
-app.directive('ngQuestion', function() {
+app.directive('ngPendingQuestion', function() {
   return {
     restrict: 'E',
     scope: {
@@ -87,4 +119,5 @@ app.directive('ngQuestion', function() {
    
   };
 });
+
 
