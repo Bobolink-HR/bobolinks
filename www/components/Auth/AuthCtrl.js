056546077496bd57a3ff8f50dd6c76e64576349a
Josh Tepei <joshuatepei94@gmail.com>
2015-06-24 16:35:15 -0700

    added login redirection functionality. removed login buttom from side menu

--- www/components/Auth/AuthCtrl.js
+++ www/components/Auth/AuthCtrl.js
@@ -1,4 +1,15 @@
-app.controller('AuthCtrl', ['$scope', function($scope) {
+app.controller('AuthCtrl', ['$scope', '$location', '$window', function($scope, $location, $window) {
   $scope.title = "Login";
-  $scope.description = "asdfa"
+
+  //redirects the user to the specified url
+  $scope.moveUrl = function(url) {
+    $window.location.href = 'http://localhost:8100/#/app' + url;
+  }
+
+  //clears the content of the input box if sent back to the login page
+  $scope.clearContents = function(element) {
+    if(!element.url) { return; }
+    element.url.text = '';
+  }
+  
 }]);
