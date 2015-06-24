7d446e431a01a36ed240253a3c7f87eb96f15425
Dianna Faulk <diannafaulk@gmail.com>
2015-06-24 14:37:39 -0700

    Add documentation, format

--- www/components/NewForum/NewForumCtrl.js
+++ www/components/NewForum/NewForumCtrl.js
@@ -1,18 +1,21 @@
-// var moment = require('moment');
 app.controller('NewForumCtrl', ['$scope', 'ForumsFactory', function($scope, ForumsFactory) {
   $scope.title = 'Create New Forum';
 
   $scope.newForum = {};
 
   $scope.addForum = function(newForum) {
-    console.log('BEFORE newForum: ', newForum)
     $scope.newForum.creatorID = "simplelogin:1"; //Update this to be the actual user ID
     $scope.newForum.createdAt = JSON.stringify(new Date().toString());
-    $scope.newForum.start = moment(newForum.startDate).toString().slice(0,16).concat( newForum.start.toString().slice(16,34) );
+
+    // Convert start to the user input date and time as a string
+    $scope.newForum.start = newForum.startDate.toString().slice(0,16).concat( newForum.start.toString().slice(16,33) );
     delete $scope.newForum.startDate;
-    $scope.newForum.end = moment(newForum.endDate).toString().slice(0,16).concat( newForum.end.toString().slice(16,34) );
+
+    // Convert end to the user input date and time as a string
+    $scope.newForum.end = newForum.endDate.toString().slice(0,16).concat( newForum.end.toString().slice(16,33) );
     delete $scope.newForum.endDate;
-    console.log('AFTER newForum: ', newForum);
+
+    // Save the forum to Firebase
     ForumsFactory.saveForum(newForum).then(function(ref) {
       var id = ref.key();
       // $scope.forum = ForumsFactory.getForum(id);
@@ -22,3 +25,4 @@ app.controller('NewForumCtrl', ['$scope', 'ForumsFactory', function($scope, Foru
     });
   }
 }]);
+
