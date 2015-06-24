774d4785c39897b04ceb4ec6f10dfe57f5ca3e0c
Dianna Faulk <diannafaulk@gmail.com>
2015-06-24 13:28:36 -0700

    Add custom start and end dates and times to forum creation

--- www/components/NewForum/NewForumCtrl.js
+++ www/components/NewForum/NewForumCtrl.js
@@ -1,17 +1,18 @@
+// var moment = require('moment');
 app.controller('NewForumCtrl', ['$scope', 'ForumsFactory', function($scope, ForumsFactory) {
   $scope.title = 'Create New Forum';
 
   $scope.newForum = {};
 
   $scope.addForum = function(newForum) {
-    console.log('newForum: ', newForum)
+    console.log('BEFORE newForum: ', newForum)
     $scope.newForum.creatorID = "simplelogin:1"; //Update this to be the actual user ID
     $scope.newForum.createdAt = JSON.stringify(new Date().toString());
-    // THIS IS WHERE THE DATES AND TIMES WOULD BE MODIFIED
-    // $scope.newForum.date = JSON.stringify(newForum.date.toString());
-    // $scope.newForum.start = JSON.stringify(newForum.start);
-    // $scope.newForum.end = JSON.stringify(newForum.end);
-    console.log('scope.newForum', $scope.newForum);
+    $scope.newForum.start = moment(newForum.startDate).toString().slice(0,16).concat( newForum.start.toString().slice(16,34) );
+    delete $scope.newForum.startDate;
+    $scope.newForum.end = moment(newForum.endDate).toString().slice(0,16).concat( newForum.end.toString().slice(16,34) );
+    delete $scope.newForum.endDate;
+    console.log('AFTER newForum: ', newForum);
     ForumsFactory.saveForum(newForum).then(function(ref) {
       var id = ref.key();
       // $scope.forum = ForumsFactory.getForum(id);
