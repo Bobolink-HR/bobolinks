299919a6eb0095179145bdb67da605291f215468
Josh Tepei <joshuatepei94@gmail.com>
2015-06-25 22:29:58 -0700

    changes to Auth/HomeCtrl.js

--- www/components/Auth/HomeCtrl.js
+++ www/components/Auth/HomeCtrl.js
@@ -3,7 +3,7 @@ app.controller('HomeCtrl', ['$scope', '$location', '$window', function($scope, $
 
   //redirects the user to the specified url
   $scope.moveUrl = function(url) {
-    $window.location.href = 'http://localhost:8100/#/app' + url;
+    $window.location.href = 'http://localhost:8100/#/app/forum/' + url;
   }
 
   //clears the content of the input box if sent back to the login page
