e10df472fb987da4a7f3b7044cc5d08e959b27c2
Jason Martin <jason.carter.martin@gmail.com>
2015-06-29 10:18:01 -0700

    Fixed ref to localhosts

--- www/components/Auth/HomeCtrl.js
+++ www/components/Auth/HomeCtrl.js
@@ -2,12 +2,6 @@ app.controller('HomeCtrl', ['$scope', '$location', '$window', 'Auth', function($
   $scope.title = "Home";
 
   $scope.loggedIn = !!Auth.getAuth();
-
-  //redirects the user to the specified url
-  // $scope.moveUrl = function(forumCode) {
-  //   $window.location.href = 'http://localhost:8100/#/app/forum/' + forumCode;
-  // };
-
   //clears the content of the input box if sent back to the login page
   $scope.clearContents = function(element) {
     if(!element.url) { return; }
