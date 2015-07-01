e777697a37565578b8554b61313e973462a1cca8
Devon <devonharvey@gmail.com>
2015-07-01 14:56:02 -0700

    (feat)Respond to poll view shows the current poll

--- www/components/RespondToPoll/RespondToPollCtrl.js
+++ www/components/RespondToPoll/RespondToPollCtrl.js
@@ -1,20 +1,5 @@
 app.controller('RespondToPollCtrl', ['$scope', '$rootScope', 'ForumsFactory', '$stateParams', function($scope, $rootScope, ForumsFactory, $stateParams) {
-  
-  $scope.title = 'Respond To Poll';
-
-  // Create empty newPoll object on $scope and set starting rank to 0
-  $scope.newPoll = {};
-  $scope.newPoll.rank = 0;
-
-  $scope.submitPoll = function() {
-
-    //if text has been inputted in the Poll input
-    if ($scope.newPoll.text !== undefined) {
-      ForumsFactory.addPoll($stateParams.forumKey, $scope.newPoll);
-      // Go back to the forum view
-      $rootScope.goBack();
-    }
-  };
+  $scope.poll = ForumsFactory.getPolls($stateParams.forumKey);
 
 }]);
 
