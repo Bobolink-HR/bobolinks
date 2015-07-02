f78f6ea839478fd2cff14a85c10cb14a1059c694
Devon <devonharvey@gmail.com>
2015-07-01 19:20:11 -0700

    (feat)Add radio buttons to respond to poll view

--- www/components/RespondToPoll/RespondToPollCtrl.js
+++ www/components/RespondToPoll/RespondToPollCtrl.js
@@ -1,8 +1,16 @@
 app.controller('RespondToPollCtrl', ['$scope', '$rootScope', 'ForumsFactory', '$stateParams', function($scope, $rootScope, ForumsFactory, $stateParams) {
   $scope.poll = ForumsFactory.getPolls($stateParams.forumKey);
+  $scope.response = 0;
+
+  $scope.setResponse = function(value) {
+    $scope.response = value;
+  }
 
   $scope.respond = function() {
-    $rootScope.goBack();
+    if($scope.response !== 0) {
+      console.log($scope.response);
+      $rootScope.goBack();
+    }
   };
 
 
